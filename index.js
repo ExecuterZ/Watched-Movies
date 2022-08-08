e4b25b053063d84fba3dad95ea0aba872c89d132
Eugeny <lokomas@inbox.ru>
2022-08-08 11:36:40 +0700

    question about lastfilm and output information

--- index.js
+++ index.js
@@ -1,12 +1,21 @@
 "use strict";
 
-const numberOfFilms = prompt('Сколько фильмов вы уже посмотрели?');
-console.log('Всего просмотрено фильмов = ' + numberOfFilms);
+const numberOfFilms = prompt('Сколько фильмов вы уже посмотрели?', '');
 
-const personalMovieDB = {
-    count: "Всего просмотрено фильмов =" + numberOfFilms,
-    movies: "",
-    actors: "",
+let personalMovieDB = {
+    count: "Всего просмотрено фильмов = " + numberOfFilms,
+    movies: {
+
+    },
+    actors: {
+
+    },
     genres: [],
     privat: false
 };
+
+let lastFilm = prompt('Какой был один из последний просмотренныых фильмов?', '');
+let markLastFilm = prompt('На сколько оцените его?', '');
+
+personalMovieDB.movies = lastFilm + " : " + markLastFilm;
+console.log(personalMovieDB);
