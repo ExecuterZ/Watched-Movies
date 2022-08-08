7439784d1aa254dbee864da9c3ee03ca7e726c2a
Eugeny <lokomas@inbox.ru>
2022-08-08 11:26:04 +0700

    add personalMovieDB

--- index.js
+++ index.js
@@ -2,3 +2,11 @@
 
 const numberOfFilms = prompt('Сколько фильмов вы уже посмотрели?');
 console.log('Всего просмотрено фильмов = ' + numberOfFilms);
+
+const personalMovieDB = {
+    count: "Всего просмотрено фильмов =" + numberOfFilms,
+    movies: "",
+    actors: "",
+    genres: [],
+    privat: false
+};
