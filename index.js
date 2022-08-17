1bf54948f7c41769466e96c7d698e68572bb1d41
Eugeny <lokomas@inbox.ru>
2022-08-17 15:51:10 +0700

    condition PersonalMovieDBcount

--- index.js
+++ index.js
@@ -3,7 +3,7 @@
 const numberOfFilms = prompt('Сколько фильмов вы уже посмотрели?', '');
 
 let personalMovieDB = {
-    count: "Всего просмотрено фильмов = " + numberOfFilms,
+    count: numberOfFilms,
     movies: {
 
     },
@@ -12,14 +12,23 @@ let personalMovieDB = {
     },
     genres: [],
     privat: false
-};
+                        };
+
 let i = 0;
 for (i = 0; i<2; i++) {
 const a = prompt('Какой был один из просмотренных фильмов?',''),
     b = +prompt('На сколько оцените его?', '');
-    if (a, b === null) {--i;}
+    if (a === null) {--i;}
     if (a, b == '') {--i;}
     if (b === NaN) {--i;}
     if (a.length > 50) {--i;}
     personalMovieDB.movies[a] = 'оценка = ' + b;}
-console.log(personalMovieDB); 
+    console.log(personalMovieDB); 
+
+    if (personalMovieDB.count <10) {
+        console.log('Просмотрено довольно мало фильмов');
+    } else if (personalMovieDB.count >=10 && personalMovieDB.count <30) {
+        console.log('Вы классический зритель');
+    } else if (personalMovieDB.count > 30) {
+        console.log('Вы киноман');
+    } else {console.log('Произошла ошибка!');}
