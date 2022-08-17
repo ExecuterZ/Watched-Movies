5617925cb77cbd8c1720a848d29294fb67e5407f
Eugeny <lokomas@inbox.ru>
2022-08-17 15:08:42 +0700

    Add limitation in unput about films

--- index.js
+++ index.js
@@ -13,9 +13,13 @@ let personalMovieDB = {
     genres: [],
     privat: false
 };
-
-for (let i = 0; i<2; i++) {
+let i = 0;
+for (i = 0; i<2; i++) {
 const a = prompt('Какой был один из просмотренных фильмов?',''),
-    b = prompt('На сколько оцените его?', '');
+    b = +prompt('На сколько оцените его?', '');
+    if (a, b === null) {--i;}
+    if (a, b == '') {--i;}
+    if (b === NaN) {--i;}
+    if (a.length > 50) {--i;}
     personalMovieDB.movies[a] = 'оценка = ' + b;}
 console.log(personalMovieDB); 
